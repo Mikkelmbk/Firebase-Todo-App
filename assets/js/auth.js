24d3496946475dcb055d221f75609d10d280ab2f
Mikkel Back Koll <217236@RTS-365.dk>
2019-10-29 14:14:22 +0100

    man kan nu oprette sig og der bliver lavet eller opdateret en collection der hedder users i firebase databasen

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -31,14 +31,21 @@ signupForm.addEventListener('submit', (event) => {
 	signupErrorMessage.innerHTML = "";
 	let allowSignup = true;
 
+
+	const name = signupForm.Name.value;
 	const email = signupForm.Email.value;
 	const password = signupForm.Password.value;
 	const repeatPassword = signupForm.repeatPassword.value;
 
+	signupForm.Name.style.backgroundColor = "white";
 	signupForm.Email.style.backgroundColor = "white";
 	signupForm.Password.style.backgroundColor = "white";
 	signupForm.repeatPassword.style.backgroundColor = "white";
 
+	if (name == "") {
+		allowSignup = false;
+		signupForm.Name.style.backgroundColor = "red";
+	}
 	if (email == "") {
 		allowSignup = false;
 		signupForm.Email.style.backgroundColor = "red";
@@ -58,13 +65,18 @@ signupForm.addEventListener('submit', (event) => {
 	}
 
 
-
 	if (allowSignup) {
 
 		auth.createUserWithEmailAndPassword(email, password)
 			.then((cred) => {
 				console.log(cred);
+				return db.collection('users').doc(cred.user.uid).set({
+					fullname: name
+				})
+			})
+			.then(() => {
 				signupForm.reset();
+
 			})
 			.catch((err) => {
 				signupErrorMessage.innerHTML = err.message;
@@ -72,7 +84,6 @@ signupForm.addEventListener('submit', (event) => {
 
 	}
 })
-
 
 auth.onAuthStateChanged((user) => {
 	console.log("This is User:", user)
@@ -98,12 +109,25 @@ auth.onAuthStateChanged((user) => {
 		}, (error) => {
 			console.log(error.message)
 		});
+
+		db.collection('users').doc(user.uid).get()
+			.then((doc) => {
+				if (doc.data().fullname) {
+					document.querySelector('.full-name').innerHTML = doc.data().fullname;
+					document.querySelector('.user-email').innerHTML = user.email;
+
+				}
+			})
+
 		loginForm.classList.add('hidden');
 		signupForm.classList.add('hidden');
 		logoutBtn.classList.remove('hidden');
 		form.classList.remove('hidden');
 	}
-	else{
+	else {
+
+		document.querySelector('.full-name').innerHTML = "";
+		document.querySelector('.user-email').innerHTML = "";
 		loginForm.classList.remove('hidden');
 		signupForm.classList.remove('hidden');
 		logoutBtn.classList.add('hidden');
